4fba3a601e7e0cb0a7ec14746d6e3c5b65752b2c
ggjiuw <herezoom1231@gmail.com>
2023-09-20 11:07:31 +0300

    I modified the code to raise errors if there is no producer

--- car_class.py
+++ car_class.py
@@ -13,21 +13,21 @@ class Automobile:
     def __str__(self):
         if self.brand == None:
             raise NameError('The car has no brand!')
+        if self.producer == None:
+            raise NameError('The car has no probucer!')
         return f'\nProducer: {self.producer}\nCar name: {self.name}\nCar Brand: {self.brand}'
     
     __repr__ = __str__
 
 first_car = Automobile('Tesla')
 second_car = Automobile('Nissan')
-
 first_car.brand = 'Cybertruck'
 second_car.brand = 'Juke'
-
-first_car.fuel_consumption = 0.0 # because electric
-second_car.fuel_consumption = 2.4
 
 first_car.producer = 'Tesla'
 second_car.producer = '日産自動車株式会社' # this is the first thing I found
 
+first_car.fuel_consumption = 0.0 # because electric
+second_car.fuel_consumption = 2.4
 # print(first_car)
 # print(second_car)
